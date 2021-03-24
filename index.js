012a591ac8118672c75f4ecfea8a82dee4e6d6eb
shrek of <lemadster@gmail.com>
2021-03-24 12:54:42 +0000

    .env file done

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
 const express = require("express")
 const app = express()
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
 app.get("/", (req, res) => {
- res.send("hello hell!")
+	res.send("hello hell!")
 })
 
 app.listen(3000, () => {
- console.log("hey lol")
+	console.log("hey lol")
 })
-let Discord = require("discord.js");
-let client = new Discord.Client();
-client.on("message", message => {
-if (message.content === "reaction time quite fast") {
-message.channel.send("the fastest!")
-}
-if(message.content === ">help") {
- let embed = new Discord.MessageEmbed()
- .setTitle("Welcome to nyo!")
- .setDescription("Jobs, Shopping, Fun")
- .setColor("Red")
- .setFooter("Type in >help(Whatever you need help with goes here)")
- message.channel.send(embed)
-}
-if(message.content === ">help job") {
-  let embed = new Discord.MessageEmbed()
-  .setTitle("The job command is a good way to get currency")
-  .setDescription("Some available jobs are:Miner, Internet troll, Robber, Police officer, and Uber Driver.")
-  .setColor("Red")
-  .setFooter("You can work as a different job everyday.")
-  message.channel.send(embed)
 
-}if(message.content === ">work miner") {
- let embed = new Discord.MessageEmbed()
- .setTitle("Work for miner")
- .setDescription("Hurry! type in I found diamonds!")
- .setColor("RANDOM")
- .setFooter("Go before someone steals them!")
- message.channel.send(embed)
-}
-if(message.content === "I found diamonds!") {
- let embed = new Discord.MessageEmbed()
- .setTitle("gg")
- .setDescription("niceeeeee you got the diamonds!")
- .setColor("RANDOM")
- .setFooter("You earned 2000 coins from your hard work.")
- message.channel.send(embed)
-}
+client.on("message", message => {
+	if (message.content === "secret command moment") {
+		message.channel.send("nice, you found it. Doesn't really matter because you got nothing, but congrats! here is a pat on the back :hand_splayed:")
+	}
+	if (message.content === ">help") {
+		let embed = new Discord.MessageEmbed()
+			.setTitle("Welcome to nyo!")
+			.setDescription("Jobs, Shopping, Fun")
+			.setColor("Red")
+			.setFooter("Type in >help(Whatever you need help with goes here)")
+		message.channel.send(embed)
+	}
+	if (message.content === ">help job") {
+		let embed = new Discord.MessageEmbed()
+			.setTitle("The job command is a good way to get currency")
+			.setDescription("Some available jobs are:Miner, Internet troll, Robber, Police officer, and Uber Driver.There are different levels for each job. The higher level you do the harder it is but the more you get paid.")
+			.setColor("Red")
+			.setFooter("You can work as a different job everyday.")
+		message.channel.send(embed)
+
+	} if (message.content === ">work miner") {
+		let embed = new Discord.MessageEmbed()
+			.setTitle("Work for miner")
+			.setDescription("Hurry! type in I found diamonds!")
+			.setColor("RANDOM")
+			.setFooter("Go before someone steals them!")
+		message.channel.send(embed)
+	}
+	if (message.content === "I found diamonds!") {
+		let embed = new Discord.MessageEmbed()
+			.setTitle("gg")
+			.setDescription("niceeeeee you got the diamonds!")
+			.setColor("RANDOM")
+			.setFooter("You earned 2000 coins from your hard work.")
+		message.channel.send(embed)
+	}
+	if (message.content === ">work internet troll") {
+		let embed = new Discord.MessageEmbed()
+			.setTitle("How can I take down his game servers?!")
+			.setDescription("A:Spam on one tab                       B:Use a PoD                                             C:Ddos it.")
+			.setColor("Blue")
+			.setFooter("Make sure you are secretive or you will get reported.")
+		message.channel.send(embed)
+	}
 })
-client.login("ODE4NjY4MDUxNzAwMzgzNzc0.YEbaCQ.9M9rjWxIzTdiMLec46C7iv55hok")
+
+client.login(BOT_TOKEN);
